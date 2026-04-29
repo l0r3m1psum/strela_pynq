96d20ceb5f8e8ad77469b608bdd9380c53b719d3
Diego Bellani <bellanidiego@gmail.com>
2026-04-29 12:50:22 +0200

    Added fix form HP port

--- load_bitstream.sh
+++ load_bitstream.sh
@@ -6,6 +6,14 @@ set -e
 # https://github.com/amaranth-lang/amaranth/issues/519
 # write_cfgmem -force -format bin -interface smapx32 -disablebitswap -loadbit "up 0 {{name}}.bit" {{name}}.bin
 
+# https://docs.amd.com/r/en-US/ug585-zynq-7000-SoC-TRM/AXI_HP-Interface-AFI-axi_hp
+# 0xF8008000 base address
+AXI_HP0=0xF8008000         # High-Performance AXI port 0 base register
+AFI_RDCHAN_CTRL=0x00000000 # Read Channel Control Register
+AFI_WRCHAN_CTRL=0x00000014 # Write Channel Control
+# Configures both interfaces as 32bit instead of 64
+# devmem2 $(printf 0x%x $(($AXI_HP0 + $AFI_RDCHAN_CTRL))) w 0x1
+# devmem2 $(printf 0x%x $(($AXI_HP0 + $AFI_WRCHAN_CTRL))) w 0x1
 
 full_bitstream=0
 partial_bitstream=1
