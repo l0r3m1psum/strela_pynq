ca2a68ed63d082d83c75751dff333e59a8349916
Diego Bellani <bellanidiego@gmail.com>
2026-05-18 16:42:45 +0200

    Cleanup

--- process_support_archive.sh
+++ process_support_archive.sh
@@ -4,9 +4,9 @@ set -e
 
 support_archive_name=$1
 
-xsct tools/generate_sdt.tcl "${support_archive_name}.xsa" sdt_out
-# sed '1i /dts-v1/;\n/plugin/;' sdt_out/pl.dtsi | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
-awk -f tools/make_overlay.awk sdt_out/pl.dtsi | tee /tmp/pl.dts | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
+xsct tools/generate_sdt.tcl "${support_archive_name}.xsa" /tmp/sdt_out
+# sed '1i /dts-v1/;\n/plugin/;' /tmp/sdt_out/pl.dtsi | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
+awk -f tools/make_overlay.awk /tmp/sdt_out/pl.dtsi | tee /tmp/pl.dts | dtc -@ -I dts -O dtb -o "${support_archive_name}.dtbo" -
 unzip -o "${support_archive_name}.xsa" "${support_archive_name}.bit"
 
 cat << EOF > /tmp/bitstream.bif
